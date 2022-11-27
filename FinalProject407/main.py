d13a902bad98a7a0bdf2ea8106a21ba159b2aa97
Dillon-G <djgilles@uncg.edu>
2022-11-27 15:45:21 -0500

    Added Graphing

--- FinalProject407/main.py
+++ FinalProject407/main.py
@@ -5,6 +5,7 @@
 
 import person
 import random
+import matplotlib.pyplot as plt
 totalPopulation = 1000
 populationArray = list()
 numberOfRounds = 2000
@@ -14,6 +15,9 @@ Beta = .01
 for x in range(totalPopulation):
   populationArray.append(person.Person(0,0))
 
+plot_round = []
+susceptible = []
+infected = []
 
 
 
@@ -49,9 +53,17 @@ for z in range(numberOfRounds):
         if populationArray[k].infectionStatus == 1:
             currentNumInfections += 1
     print("Round: "+str(z) +" -->" +str(currentNumInfections))
+    plot_round.append(z)
+    susceptible.append(totalPopulation - currentNumInfections)
+    infected.append(currentNumInfections)
 
 
-
+plt.xlabel('Round')
+plt.ylabel('Population')
+plt.plot(plot_round, susceptible, label = "Susceptible", color = "blue")
+plt.plot(plot_round, infected, label = "Infected", color = "red")
+plt.legend()
+plt.show()
 
 
 def print_hi(name):
