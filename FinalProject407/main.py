2f48fa396f32e0062445efdb971fe84fe22c3830
ALEXECKERT93 <78118345+ALEXECKERT93@users.noreply.github.com>
2022-11-28 13:49:31 -0500

    fixed part D

--- FinalProject407/main.py
+++ FinalProject407/main.py
@@ -12,13 +12,17 @@ populationArray = list()
 numberOfRounds = 2000
 lengthOfInfection = 20
 lengthOfImmunity = 10
-vaccineSuccessRate = .95
+vaccineSuccessRate = 1
 vaccineIntroductionRound = 50
-vaccineAcceptanceRate = .95
+vaccineDecrementValue = .00
+vaccineAcceptanceRate = 1
 Alpha = .01
 Beta = .01
 for x in range(totalPopulation):
-  populationArray.append(person.Person(0,0,0,0,random.random()))
+    # change last parameter to static int to vaccinate all on one round... currently each person generates a random round they will get vaccinated on... you can also set a minimum round for the vaccine to appear
+  populationArray.append(person.Person(0,0,0,0,random.random(),vaccineSuccessRate,random.randint(50,numberOfRounds - 1)))
+
+
 
 plot_round = []
 susceptible = []
@@ -41,7 +45,7 @@ for z in range(numberOfRounds):
                 if random.random() <= Alpha:
                     if random.random() <= Beta:
                         if populationArray[y].infectionStatus == 0:
-                            if z < vaccineIntroductionRound or populationArray[y].vaccineStatus > vaccineAcceptanceRate or random.random() >= vaccineSuccessRate:
+                            if z < populationArray[y].vaccineIntroductionRound or populationArray[y].vaccineStatus > vaccineAcceptanceRate or random.random() >= populationArray[y].vaccineSuccessRate:
 
                                 populationArray[y].infectionStatus = 1
                                 populationArray[y].turnsRemaining = -1
@@ -50,7 +54,7 @@ for z in range(numberOfRounds):
                            #negative one turn remaining avoids the node infecting others until the next round
                             populationArray[y].turnsRemaining = -1
                         elif populationArray[x].infectionStatus == 0:
-                            if z < vaccineIntroductionRound or populationArray[x].vaccineStatus > vaccineAcceptanceRate or random.random() >= vaccineSuccessRate:
+                            if z < populationArray[x].vaccineIntroductionRound or populationArray[x].vaccineStatus > vaccineAcceptanceRate or random.random() >= populationArray[x].vaccineSuccessRate:
                                 populationArray[x].infectionStatus = 1
                                 populationArray[x].turnsRemaining = -1
                                 populationArray[y].infectionsCaused += 1
@@ -76,6 +80,8 @@ for z in range(numberOfRounds):
             if populationArray[x].immunityRemaining <= 0:
                 populationArray[x].infectionStatus = 0
                 populationArray[x].turnsRemaining = -1
+        if z > populationArray[x].vaccineIntroductionRound:
+            populationArray[x].vaccineSuccessRate -= vaccineDecrementValue
         x += 1
     for k in range(totalPopulation):
 
--- FinalProject407/person.py
+++ FinalProject407/person.py
@@ -1,9 +1,11 @@
 class Person:
 
-  def __init__(self, infectionStatus, turnsRemaining, immunityRemaining,infectionsCaused,vaccineStatus):
+  def __init__(self, infectionStatus, turnsRemaining, immunityRemaining,infectionsCaused,vaccineStatus,vaccineSuccessRate,vaccineIntroductionRound):
     # 0 = uninfected, 1 = infected, 2 = immune
     self.infectionStatus = infectionStatus
     self.turnsRemaining = turnsRemaining
     self.immunityRemaining = immunityRemaining
     self.infectionsCaused = infectionsCaused
     self.vaccineStatus = vaccineStatus
+    self.vaccineSuccessRate = vaccineSuccessRate
+    self.vaccineIntroductionRound = vaccineIntroductionRound
